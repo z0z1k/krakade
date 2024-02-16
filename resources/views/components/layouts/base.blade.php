f71061c8aad547817711e69dcff2069f631c81ac
z0z1k <z0z1k8k@gmail.com>
2024-02-16 16:27:05 +0200

    deleted valentine's day hearts

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -14,8 +14,6 @@
     <div class="wrapper">
         <header class="header">    
             <x-navbar />
-
-<canvas id='myCanvas' width='800' height='300'></canvas>
         </header>
 
         <main class="main py-3">
@@ -28,46 +26,6 @@
                     </div>
                 </div>
             </div>
-
-<script>
-const endAngle = 226;
-let canvas = document.getElementById('myCanvas');
-let context = canvas.getContext('2d');
-let counter = 60;
-let radius, tx, ty;
-context.translate(tx, ty);
-randomize();
-requestAnimationFrame(animate);
- 
-function animate() {
-  let x, y;
-  if (counter <= endAngle) {
-    let radians = Math.PI / 180 * counter;
-    y = radius * Math.sin(radians);
-    x = radius * Math.cos(radians);
-    context.fillRect(radius / 2 - x, -y, 2, 2);
-    context.fillRect(-radius / 2 + x, -y, 2, 2);
-  } else {
-    x = counter - endAngle - radius * 1.2;
-    y = counter - endAngle + radius * .71;
-    context.fillRect(x, y, 2, 2);
-    context.fillRect(-x, y, 2, 2);
-  }
-  counter = counter + 1;
-  if (counter >= endAngle + radius * 1.2) randomize();
-  requestAnimationFrame(animate);
-}
- 
-function randomize() {
-  counter = 60;
-  context.fillStyle = 'rgba(255,' + Math.floor(Math.random() * 255) + ', 255 ,1)';
-  radius = Math.random() * 100;
-  context.translate(-tx, -ty);
-  tx = Math.random() * canvas.width;
-  ty = Math.random() * canvas.height;
-  context.translate(tx, ty);
-}
-</script>
         </main>
 
         <footer class="footer">
