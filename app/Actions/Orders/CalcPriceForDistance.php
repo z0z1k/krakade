e2ee38e9b73e22fb73fab73f0154688b716d94b8
z0z1k <z0z1k8k@gmail.com>
2024-02-21 00:44:05 +0200

    info page for places, CalcPriceForDistance in action class

--- app/Actions/Orders/CalcPriceForDistance.php
+++ app/Actions/Orders/CalcPriceForDistance.php
@@ -2,14 +2,6 @@
 
 namespace App\Actions\Orders;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
-
-use Carbon\Carbon;
-
-use App\Models\User;
-use App\Models\City;
-
 class CalcPriceForDistance
 {
     public function __invoke($prices, $request)
@@ -18,7 +10,6 @@ class CalcPriceForDistance
             $price = $prices['default'];
         } else {
             foreach ($prices as $distance => $value) {
-                //dd($request->distance);
                 $price = false;
                 if ($request->distance <= floatval(str_replace(",", ".", $distance)) * 1000) {
                     $price = $value;
