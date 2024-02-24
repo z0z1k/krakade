301a6764c5fc0259d4f43efe140929a7638e1914
z0z1k <z0z1k8k@gmail.com>
2024-02-24 06:39:10 +0200

    minor updates

---
+++ app/Actions/Orders/UpdateMessage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Orders;
+
+class UpdateMessage
+{
+    public function __invoke($message)
+    {
+        if(!str_contains($message, 'Оновлення!')){
+            $message  = 'Оновлення! ' . $message;
+        }
+        
+        return $message . 1;
+    }
+}
