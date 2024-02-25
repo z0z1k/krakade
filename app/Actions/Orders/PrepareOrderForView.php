1e94d58f4e0529817465237f58a761fb2e422551
z0z1k <z0z1k8k@gmail.com>
2024-02-25 16:44:23 +0200

    rename action methods

--- app/Actions/Orders/PrepareOrderForView.php
+++ app/Actions/Orders/PrepareOrderForView.php
@@ -11,11 +11,12 @@ class PrepareOrderShowForView
     {
         $order->date = Carbon::parse($order->created_at)->format('d.m.y D');
         $order->approximate_ready_at = Carbon::parse($order->approximate_ready_at)->format('H:i');
-        $order->taken_at = Carbon::parse($order->taken_at)->format('H:i');
-        $order->delivered_at = Carbon::parse($order->delivered_at)->format('H:i');
+        $order->taken_at = $order->taken_at ? Carbon::parse($order->taken_at)->format('H:i') : '';
+        $order->delivered_at = $order->delivered_at ? Carbon::parse($order->delivered_at)->format('H:i') : '';
         $city = City::findOrFail($order->city_id)->city;
         $order->client_address = "$city, $order->address $order->address_info";
 
         return $order;
     }
 }
+
