7140f159f23acc6ba552a7a51986be80ef144737
z0z1k <z0z1k8k@gmail.com>
2024-03-19 20:05:05 +0200

    sound notification for updates

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -1,5 +1,12 @@
 var socket = new WebSocket(wsURL);
 
 socket.onmessage = function(event) {
-    location.reload();
+    var timer, sound;
+    sound = new Howl({
+        src: ['assets/sounds/notification.mp3']
+    });
+    setInterval(function(){
+        sound.play();
+    },1000);
+    setTimeout(function(){location.reload()},1200);
 };
--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -8,6 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.2/howler.core.min.js">
+    </script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/main.css', 'resources/css/loader.css'])
 </head>
 <script>
